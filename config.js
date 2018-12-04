832d8e225fcd00d9bad793b9f1b8b4ef80aff36a
v1st <brandon_m918@yahoo.com>
2018-12-03 19:44:04 -0500

    Added React-redux, setting up some beginner files

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const config = {
-  API_KEY = 'd010e5b901e1e9771f90a1e5936eca7e'  
+module.exports = {
+  API_KEY: 'd010e5b901e1e9771f90a1e5936eca7e'  
 }
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+const url = 'http://localhost/';
+const port = process.env.PORT || 5000;
+
+const config = require('./config');
+const apiKey = config.API_KEY;
+
+const searchURL = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=spider%20man&page=1&include_adult=false
+`;
+
+
+app.get('/', (req, res) => res.send('hi'));
+
+
+
+app.listen(port, () => { console.log(`App listening on ${url}${port} `); });
